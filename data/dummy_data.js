5ed7dd9006434c518538002e99ff22610e6faa2c
akifguldemir <akifguldemir@bontesoft.com>
2024-01-26 00:53:39 +0300

    dummy data

---
+++ data/dummy_data.js
@@ -0,0 +1,62 @@
+export default DUMMY_DATA =  [
+    {
+      id: 1,
+      title: "News1",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 2,
+      title: "News2",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 3,
+      title: "News3",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 4,
+      title: "News4",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 5,
+      title: "News5",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 6,
+      title: "News6",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 7,
+      title: "News7",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 8,
+      title: "News8",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 9,
+      title: "News9",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+    {
+      id: 10,
+      title: "News10",
+      content: "Lorem Ipsum",
+      image: "https://picsum.photos/200/200",
+    },
+  ];
