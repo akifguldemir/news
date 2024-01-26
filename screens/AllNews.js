7218498fd4183cda7a9bcc382ba9c17cf67c45c3
akifguldemir <akifguldemir@gmail.com>
2024-01-26 14:15:32 +0300

    aaxios

--- screens/AllNews.js
+++ screens/AllNews.js
@@ -1,11 +1,34 @@
 import { View, Text, FlatList, StyleSheet } from "react-native";
 import NewsFlatListItem from "../Components/NewsFlatListItem";
 import dummy_data from "../data/dummy_data";
+import axios from "axios";
+
 
 function renderItem(itemData) {
   return <NewsFlatListItem {...itemData.item} />;
 }
 
+function fetchNews() {
+  axios
+    .get("https://api.collectapi.com/news/getNews?country=tr&tag=general", {
+      headers: {
+        'Authorization': 'apikey 3tkOM2gx5sROLOEtdu0VSa:5LfdAuAbg9cnFhmgPL17LL',
+        'content-type': 'application/json'
+      }
+    })
+    .then(function (response) {
+      if(response.status === 200) {
+      }
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+fetchNews()
 function AllNews() {
   return (
     <View>
